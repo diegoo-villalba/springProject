3b4cda063530a0b320bf3218c09fc8bece6a15c5
Diego Villalba <diegoo.villalba@gmail.com>
2022-03-09 20:10:55 -0300

    Added fix for empty spaces in validated fields

--- src/main/java/spring/mvc/UserController.java
+++ src/main/java/spring/mvc/UserController.java
@@ -2,15 +2,32 @@ package spring.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	/*
+	 * This method trims the strings entered in the validated "Strinzxsa" fields prior to being sent to the controller.
+	 */
+	@InitBinder
+	public void myBinder(WebDataBinder binder) {
+		
+		//Property editor
+		StringTrimmerEditor trimmer = new StringTrimmerEditor(true);
+		//True: para que si tenemos un String vacío (espacio en blanco) se transforme en null
+		
+		
+		binder.registerCustomEditor(String.class, trimmer);
+	}
 
 	/*
 	 * This method shows a register form that includes: name, lastname, send button
